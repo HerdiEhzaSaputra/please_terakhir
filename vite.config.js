32002dd2f98f88b4c3f0eeb2949b901c2ad8d518
HerdiEhzaSaputra <marketing@dku.id>
2022-07-12 10:03:16 +0700

    remove firebase

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
-            'resources/js/base.js',
+            // 'resources/js/base.js',
         ]),
     ],
 });
